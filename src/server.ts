d095602102798dc22b9baa93efea440d6853df24
lucaspenteadosouza07@gmail.com <lucaspenteadosouza07@gmail.com>
2025-04-14 11:49:32 -0300

    server http criado

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,11 @@
-console.log("Lucas Penteado de Souza");
+import * as http from "http";
+
+const server = http.createServer((request: http.IncomingMessage, response: http.ServerResponse)=>{
+
+});
+
+const port = process.env.PORT;
+
+server.listen(port, ()=>{
+    console.log(`Servidor iniciado na porta ${port}`);
+});
